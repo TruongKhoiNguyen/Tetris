ea227d05a23f0c8a842d983c4dcba34560ba6087
Nguyen <truongkhoinguyen2002@gmail.com>
2021-02-26 19:42:36 +0700

    Tập hợp dependency về một file

--- Tetris.cpp
+++ Tetris.cpp
@@ -1,23 +1,4 @@
-#include <cstdlib>
-#include <cstdint>
-#include <cstdio>
-#include <cstring>
-#include <cassert>
-
-#include <SDL.h>
-#include <SDL_ttf.h>
-
-
-typedef uint8_t u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef int8_t s8;
-typedef int16_t s16;
-typedef int32_t s32;
-typedef float f32;
-typedef double f64;
-
-#include "colors.h"
+#include "include.h"
 
 #define WIDTH 10
 #define HEIGHT 22
