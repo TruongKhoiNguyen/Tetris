bb4ff07bf97db3b911d67b5e5cc1ce3b59543420
Nguyen <truongkhoinguyen2002@gmail.com>
2021-05-03 08:24:10 +0700

    Fix lỗi nhỏ

--- Sound.h
+++ Sound.h
@@ -19,20 +19,20 @@ const std::string MUSIC_LIST[] = {
 };
 const s32 MUSIC_LIST_SIZE = sizeof(MUSIC_LIST) / sizeof(MUSIC_LIST[0]);
 
-const std::string CLEAR_SOUND = "sound/clear.wav";
+//const std::string CLEAR_SOUND = "sound/clear.wav";
 const std::string FALL_SOUND = "sound/fall.wav";
 const std::string GAMEOVER_SOUND = "sound/gameover.wav";
 const std::string LINE_SOUND = "sound/line.wav";
 const std::string SELECTION_SOUND = "sound/selection.wav";
-const std::string SUCCESS_SOUND = "sound/success.wav";
+//const std::string SUCCESS_SOUND = "sound/success.wav";
 
 const std::string SOUND_EFFECT[] = {
-	CLEAR_SOUND,
+	//CLEAR_SOUND,
 	FALL_SOUND,
 	GAMEOVER_SOUND,
 	LINE_SOUND,
 	SELECTION_SOUND,
-	SUCCESS_SOUND,
+	//SUCCESS_SOUND,
 };
 const s32 SOUND_EFFECT_SIZE = sizeof(SOUND_EFFECT) / sizeof(SOUND_EFFECT[0]);
 
